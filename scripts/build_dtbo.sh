613c9a4f1b4ad8c89a99d90fcdb38e502c67e1b2
K-Black <75488232+K-Blaaaack@users.noreply.github.com>
2025-10-20 21:40:31 +0800

    上传文件

---
+++ scripts/build_dtbo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+OUT=out
+mkdir -p ${OUT}
+
+DTC=dtc
+MKDTOB=mkdtboimg
+
+# 构建 MIUI
+${DTC} -I dts -O dtb -o ${OUT}/overlay_miui.dtb dtbo/miui_touch_invert_overlay.dts
+${MKDTOB} --page_size 4096 --output ${OUT}/dtbo_fixed_miui.img ${OUT}/overlay_miui.dtb
+
+# 构建 AOSP
+${DTC} -I dts -O dtb -o ${OUT}/overlay_aosp.dtb dtbo/aosp_touch_invert_overlay.dts
+${MKDTOB} --page_size 4096 --output ${OUT}/dtbo_fixed_aosp.img ${OUT}/overlay_aosp.dtb
+
+echo "已生成 dtbo_fixed_miui.img 和 dtbo_fixed_aosp.img"
